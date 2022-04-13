41693a934f5f4245256e6c4f8b030cd3fb00d85f
alexclark_nz <hello@alexclark.co.nz>
2022-04-14 10:47:21 +1200

    Run logout and login routes through localhost locally

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -8,7 +8,7 @@ class AuthController extends Controller
 {
     public function login()
     {
-        return redirect()->to(config('star-auth.url') . '?redirectUrl=' . config('star-auth.dashboard_url'));
+        return redirect()->to($this->authAppUrl() . '?redirectUrl=' . config('star-auth.dashboard_url'));
     }
 
     public function logout()
@@ -18,6 +18,16 @@ class AuthController extends Controller
         session()->forget('access_token');
 
         // Redirect through the auth app
-        return redirect()->to(config('star-auth.url') . '/logout' . '?redirectUrl=' . config('app.url'));
+        return redirect()->to($this->authAppUrl() . '/logout' . '?redirectUrl=' . config('app.url'));
+    }
+
+    public function authAppUrl()
+    {
+        // In local dev, we want requests to run through the Docker network, but redirects to just go through localhost
+        if (env('APP_ENV') === 'local') {
+            return str_replace('host.docker.internal', 'localhost', config('star-auth.url'));
+        } else {
+            return config('star-auth.url');
+        }
     }
 }
